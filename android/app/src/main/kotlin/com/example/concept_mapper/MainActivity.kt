6b00b3cfb3d35345fbca75face62d88f599e40b3
dvirberlo <dvir.berlowitz@hotmail.com>
2022-03-06 17:26:21 +0200

    heroku first build

--- android/app/src/main/kotlin/com/example/concept_mapper/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.concept_mapper
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/themes/themes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:yaru/yaru.dart';
-
-class AppTheme {
-  static ThemeData light = yaruLight;
-  static ThemeData dark = yaruDark;
-  static ThemeMode themeMode = ThemeMode.dark;
-}
